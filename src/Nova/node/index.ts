af98ef7ed7cd3498d31bb0858e5f8d57cff1998e
Gavin Payne <gavinapayne@gmail.com>
2025-10-04 13:45:41 +0400

    v1.1.31

--- src/Nova/node/index.ts
+++ src/Nova/node/index.ts
@@ -11,7 +11,7 @@ export function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType, AI_RESULT_CHANNEL, SYSTEM_CHANNEL } = getPlatformDependencies();
 
   return {
-    packageVersion: "1.1.26",
+    packageVersion: "1.1.30",
     type: "AWSNovaSpeech",
     isService: false,
     name: "AWS Nova Speech",
